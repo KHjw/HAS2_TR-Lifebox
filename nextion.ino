26b68ae095e7542329d27d772b5bc6edf5f070a1
KHjinu <kevinlike@naver.com>
2023-07-11 14:18:49 +0900

    rfid 간소화

--- nextion.ino
+++ nextion.ino
@@ -13,13 +13,3 @@ void SendCmd(String command){           // 영문용 디스플레이 send
   }
   sendCommand(cmd.c_str());
 }
-
-void ExpSend(){
-  SendCmd("picExp.pic=2");
-  SendCmd("pgItemOpen.vExp.val=50");    // 경험치 +50 차있는걸로 기본세팅
-}
-
-void BatteryPackSend(){
-  SendCmd("picBatteryPack.pic=5");
-  SendCmd("pgItemOpen.vBatteryPack.val=3");    // 배터리팩 3개 차있는걸로 기본세팅
-}
